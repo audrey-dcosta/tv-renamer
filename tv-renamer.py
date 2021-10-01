bc7d7779bd80edf71d8155b7f956c8438d4acc83
audrey-dcosta <audrey2795@gmail.com>
2021-10-01 23:04:49 +0530

    created function to clean string for filename

--- tv-renamer.py
+++ tv-renamer.py
@@ -1,4 +1,5 @@
 import requests
+import re
 
 def get_serch_results(name):
     results = requests.get(f'https://api.tvmaze.com/search/shows?q={name}').json()
@@ -24,15 +25,21 @@ def get_episodes(season_id,series_name):
         episode_id=episode['id']
         episode_num=episode['number']
         season_num=episode['season']
-        episode_title=episode['name']
+        episode_title=clean_str(episode['name'])
         episode_name(f'{series_name}[{season_num}x{episode_num:02d}] {episode_name}')
         print(f'{series_name}[{season_num}x{episode_num:02d}] {episode_name}')
+
+def clean_str(strg):
+    cleaned_str=re.sub(r'[\\/*?<>|]',"",strg)
+    cleaned_str=re.sub(r'[:]',"-",cleaned_str)
+    cleaned_str=re.sub(r'["]',"'",cleaned_str)
+    return cleaned_str
         
 
 def main():
     # get_serch_results('girls')
     # get_seasons(23542)
-    get_episodes(109940,'girls')
+    # get_episodes(109940,'girls')
 
 if __name__ =="__main__":
     main()
