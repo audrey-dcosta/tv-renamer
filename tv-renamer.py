71fbba474c347b36dccf211ffa0cb3a4a7da85b7
audrey-dcosta <audrey2795@gmail.com>
2021-10-01 21:20:51 +0530

    added function to get seasons in a series

--- tv-renamer.py
+++ tv-renamer.py
@@ -3,11 +3,23 @@ import requests
 def get_serch_results(name):
     results = requests.get(f'https://api.tvmaze.com/search/shows?q={name}').json()
     for res in results:
-        print(res['show']['name'])
+        series_id=res['show']['id']
+        series_name=res['show']['name']
+        print(f'{series_id} {series_name}')
+    
+
+def get_seasons(series_id):
+    seasons=requests.get(f'https://api.tvmaze.com/shows/{series_id}/seasons').json()
+    for season in seasons:
+        season_id=season['id']
+        season_num=season['number']
+        num_of_episodes=season['episodeOrder']
+        print(f'{season_id} {season_num} {num_of_episodes}')
         
 
 def main():
-    get_serch_results('girls')
+    # get_serch_results('girls')
+    get_seasons(23542)
 
 if __name__ =="__main__":
     main()
