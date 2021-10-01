b8cf8aedbec5bfc2f533e049fe3bbf1928815cb9
audrey-dcosta <audrey2795@gmail.com>
2021-10-01 21:33:43 +0530

    function to get episodes

--- tv-renamer.py
+++ tv-renamer.py
@@ -5,7 +5,8 @@ def get_serch_results(name):
     for res in results:
         series_id=res['show']['id']
         series_name=res['show']['name']
-        print(f'{series_id} {series_name}')
+        series_premiered=res['show']['premiered']
+        print(f'{series_id} {series_name}({series_premiered})')
     
 
 def get_seasons(series_id):
@@ -15,11 +16,19 @@ def get_seasons(series_id):
         season_num=season['number']
         num_of_episodes=season['episodeOrder']
         print(f'{season_id} {season_num} {num_of_episodes}')
+
+def get_episodes(season_id):
+    episodes=requests.get(f'https://api.tvmaze.com/seasons/{season_id}/episodes').json()
+    for episode in episodes:
+        episode_id=episode['id']
+        episode_name=episode['name']
+        print(f'{episode_id} {episode_name}')
         
 
 def main():
     # get_serch_results('girls')
-    get_seasons(23542)
+    # get_seasons(23542)
+    get_episodes(109940)
 
 if __name__ =="__main__":
     main()
